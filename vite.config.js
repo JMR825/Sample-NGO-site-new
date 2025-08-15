f3a9423d42ca7aa4d84a0d201436b80dc571af3d
JMR825 <mdesktop262@gmail.com>
2025-08-15 19:31:01 +0530

    -m

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sample-NGO-site-new/',
+  base: '/Sample-NGO-site/',
   // ... other config`uration options
 
 })
